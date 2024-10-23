96201f7f943582692a828cb6ab2b48d23ad2e90b
amalraj78 <amalraj.johncharlesvaratharajan@ensea.fr>
2024-10-23 14:37:32 +0200

    aeroport

--- tp_airport/src/Aeroport.java
+++ tp_airport/src/Aeroport.java
@@ -1,2 +1,32 @@
 public class Aeroport {
+    private String IATA;
+    private String Name;
+    private String country;
+    private double latitude;
+    private double longitude;
+
+    public String getIATA() {
+        return IATA;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public Aeroport(String IATA, String name, String country, double latitude, double longitude) {
+        this.IATA = IATA;
+        this.Name = name;
+        this.country = country;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "L'aéroport de "+Name+", situé en "+country+", en "+latitude+", "+longitude+", et le code IATA est :"+IATA;
+    }
 }
---
+++ tp_airport/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Aeroport test = new Aeroport("111","test", "France", 1010, 1111);
+        System.out.println(test.toString());
+    }
+
+}
+
